0e9bef5c448d4878de523a43d185b1d07b4ddba6
Alex <alex.mahinko@gmail.com>
2017-05-03 14:28:50 -0400

    win conditions and game over state implemented

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 var isX = true;
+var gameover = false;
 
 $(".gamecell").click(function() {
+if (gameover == true) {
+    return false
+}
+else {
+
   var tgt = $(event.target);
   if (tgt.is('p')){
     return false;
@@ -9,6 +15,8 @@ $(".gamecell").click(function() {
   setScore();
   playerSwitch();
   tgt.attr('class', 'gamecellClicked');
+  winCheck();
+}
 })
 
 
@@ -35,13 +43,90 @@ function setCell() {
 }
 
 function setScore() {
-  if (isX === true){
-    Xscore.push(event.target.id);
+  if (isX == true){
+    score[event.target.id] = 1;
   }
   else {
-    Oscore.push(event.target.id);
+    score[event.target.id] = 10;
   }
 }
 
-var Xscore = [];
-var Oscore = [];
+function xWin(){
+  $('#xwin_message').html('<h1>Player 1 wins!</h1>');
+  gameover = true;
+}
+
+function oWin(){
+  $('#owin_message').html('<h1>Player 2 wins!</h1>');
+  gameover = true;
+}
+
+
+var score = {
+  "1x1": null,
+  "1x2": null,
+  "1x3": null,
+  ///
+  "2x1": null,
+  "2x2": null,
+  "2x3": null,
+  ///
+  "3x2": null,
+  "3x1": null,
+  "3x3": null,
+};
+
+function winCheck() {
+  if (score["1x1"] + score['1x2'] + score ['1x3'] == 3) {
+    xWin();
+  }
+  else if (score["2x1"] + score['2x2'] + score ['3x3'] == 3) {
+    xWin();
+  }
+  else if (score["3x1"] + score['3x2'] + score ['3x3'] == 3) {
+    xWin();
+  }
+
+  if (score["1x1"] + score['2x1'] + score ['3x1'] == 3) {
+    xWin();
+  }
+  else if (score["1x2"] + score['2x2'] + score ['3x2'] == 3) {
+    xWin();
+  }
+  else if (score["1x3"] + score['2x3'] + score ['3x3'] == 3) {
+    xWin();
+  }
+  ///
+  if (score["1x1"] + score['2x2'] + score ['3x3'] == 3) {
+    xWin();
+  }
+  else if (score["1x3"] + score['2x2'] + score ['1x3'] == 3) {
+    xWin();
+  }
+  ///
+  if (score["1x1"] + score['1x2'] + score ['1x3'] == 30) {
+    oWin();
+  }
+  else if (score["2x1"] + score['2x2'] + score ['3x3'] == 30) {
+    oWin();
+  }
+  else if (score["3x1"] + score['3x2'] + score ['3x3'] == 30) {
+    oWin();
+  }
+
+  if (score["1x1"] + score['2x1'] + score ['3x1'] == 30) {
+    oWin();
+  }
+  else if (score["1x2"] + score['2x2'] + score ['3x2'] == 30) {
+    oWin();
+  }
+  else if (score["1x3"] + score['2x3'] + score ['3x3'] == 30) {
+    oWin();
+  }
+  if (score["1x1"] + score['2x2'] + score ['3x3'] == 30) {
+    oWin();
+  }
+  else if (score["1x3"] + score['2x2'] + score ['1x3'] == 30) {
+    oWin();
+  }
+}
